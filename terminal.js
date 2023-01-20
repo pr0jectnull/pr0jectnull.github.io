7910494ccbeae2eed7c3d703628fe1d2adc98ab8
gmanandmarbles <119905567+gmanandmarbles@users.noreply.github.com>
2023-01-20 10:53:50 -0700

    Update terminal.js

--- terminal.js
+++ terminal.js
@@ -119,24 +119,7 @@ register_cmd("update", function(cmd) {
     }
 
 });
-log("Welcome to a new online simulated terminal!");
-// Example Command - Hey
-register_cmd("hey", function (cmd) {
-    var parameters = cmd.split(" ").slice(1);
-    for (var i = 0; i < parameters.length; i++) {
-        block_log("Hello " + parameters[i]);
-    }
+register_cmd("help", function(cmd) {
+    block_log("Welcome to the help menu");
 });
-// Example Command - Sum
-register_cmd("sum", function(cmd){
-	var parameters = cmd.split(" ").slice(1);
-	var sum = 0;
-    for (var i = 0; i < parameters.length; i++) {
-        sum += parseInt(parameters[i]);
-    }
-    block_log("Sum: " + sum);
-});
-register_cmd("help", function(cmd){
-	
-    block_log("Welcome to the 'Help' Menu");
-});
+
