8728d241bff2453081c26deee044e46f41c646ba
gmanandmarbles <119905567+gmanandmarbles@users.noreply.github.com>
2023-01-27 14:43:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
-const text = document.getElementById("text");
+
 const messages = ["Welcome to project null", "We are glad you are here", "We released something onto the world that we cannot control", "Project Null"];
+let index = 0;
+const text = document.getElementById("text");
 let index = 0;
 
 function displayText() {
-  text.innerText = messages[index];
-  let characters = text.getElementsByTagName("span");
-  for (let i = 0; i < characters.length; i++) {
-    if (Math.random() < 0.1) {
-      characters[i].classList.add("glitch");
-    } else {
-      characters[i].classList.remove("glitch");
-    }
-  }
+  text.innerHTML = messages[index];
 }
-
 setInterval(() => {
   displayText();
   index = (index + 1) % messages.length;
-}, 5000);
+}, 3000);
